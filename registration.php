b66c76909054929558bff63170e47e20df1d0928
The Icon Ecommerce <theiconnz@gmail.com>
2017-11-26 07:56:53 +1300

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * GSuite (https://theicon.co.nz/)
+ *
+ * Module
+ *
+ * PHP version 7
+ *
+ * @category Module
+ * @package  Theicon
+ * @author   Don Nuwinda <nuwinda@gmail.com>
+ * @license  GPL https://theicon.co.nz
+ * @link     https://theicon.co.nz
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+  \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Theiconnz_GSuite', __DIR__
+  );
